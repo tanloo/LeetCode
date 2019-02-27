d8506fcaf80fb321d4ae5ffe518f3798a8e6e1fa
tlzwd135 <tlzwd135@163.com>
2019-02-27 18:46:38 +0800

    format

--- 26.remove-duplicates-from-sorted-array.java
+++ 26.remove-duplicates-from-sorted-array.java
@@ -53,8 +53,9 @@
  */
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0)
+        if (nums.length == 0){
             return 0;
+        }
         int i = 0;
         for (int j = 1; j < nums.length; j++) {
             if (nums[j] != nums[i]) {
